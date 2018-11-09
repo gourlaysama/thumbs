75cede9b3e487b58b54293653d94afe32f544152
Antoine Gourlay <antoine@gourlay.fr>
2018-11-09 18:14:04 +0100

    never fail build.rs

--- build.rs
+++ build.rs
@@ -1,19 +1,21 @@
 use std::process;
 
-fn main() -> Result<(), std::io::Error> {
+fn main() {
     let is_tag = process::Command::new("git")
         .args(&["describe", "--exact-match", "--tags"])
         .stderr(process::Stdio::null())
-        .status().map(|s| s.success()).unwrap_or(false);
+        .status()
+        .map(|s| s.success())
+        .unwrap_or(false);
 
     if !is_tag {
-        let out = process::Command::new("git")
+        if let Ok(out) = process::Command::new("git")
             .args(&["rev-parse", "--short=7", "HEAD"])
-            .output()?;
-        let hash = String::from_utf8_lossy(&out.stdout);
+            .output()
+        {
+            let hash = String::from_utf8_lossy(&out.stdout);
 
-        println!("cargo:rustc-env=THUMB_GIT_HASH={}", hash.trim());
+            println!("cargo:rustc-env=THUMB_GIT_HASH={}", hash.trim());
+        }
     }
-
-    Ok(())
 }
